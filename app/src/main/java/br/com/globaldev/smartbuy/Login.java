49634b69e6b81ecfbb7b328028fe280352551e31
Renan Xavier <renanxavier@Outlook.com>
2019-06-18 09:16:53 -0300

    Ordem de abertura de atividades

--- app/src/main/java/br/com/globaldev/smartbuy/Login.java
+++ app/src/main/java/br/com/globaldev/smartbuy/Login.java
@@ -1,28 +1,39 @@
 package br.com.globaldev.smartbuy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+
+    private EditText editTextLoginEmail, editTextLoginPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Login");
+
+        editTextLoginEmail = findViewById(R.id.editTextLoginEmail);
+        editTextLoginPass = findViewById(R.id.editTextLoginPass);
+
+        findViewById(R.id.buttonLogin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editTextLoginEmail.getText().toString().equals("smart@buy.com") && editTextLoginPass.getText().toString().equals("123")){
+                    startActivity(new Intent(Login.this, MainActivity.class));
+                }else {
+                    Toast.makeText(Login.this, "Falha ao realizar login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-        if (id == 16908332) {
-            this.finish();
-        }
-        return true;
-    }
 }
